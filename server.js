cd12b8155ceecccc3f1bd0ffa00218391f1ed659
NAJI2329 <be1512t6@gmail.com>
2022-01-25 05:01:01 +0900

    Websockect test

--- server.js
+++ server.js
@@ -4,6 +4,20 @@ const app = express();
 // Init Middleware
 app.use(express.json());
 
+
+
+
+
+
+
+const WebSocket = require('ws');
+
+
+
+
+
+
+
 const { request, gql } = require('graphql-request');
 
 const CANDLE_SUBSCRIPTION = gql`{
@@ -20,9 +34,46 @@ const CANDLE_SUBSCRIPTION = gql`{
 }
 `;
 
-request('https://api.thegraph.com/subgraphs/name/matrixswap/matrixswap-perp', CANDLE_SUBSCRIPTION).then((data) => console.log(data))
+let prevData = null;
 
+async function getdata() {
+  try {
+    const data = await request('https://api.thegraph.com/subgraphs/name/matrixswap/matrixswap-perp', CANDLE_SUBSCRIPTION);
+    console.log(data.candles[0].timestamp);
+    if(prevData == null) {
+      prevData = data;
+      console.log(data);
+    }
+  
+    if(prevData.candles[0].timestamp != data.candles[0].timestamp) {
+      prevData = data;
+      console.log(data);
+    }
+    
+  } catch (error) {
+    
+  }
+}
+
+
+
+const client = new WebSocket('wss://example.com:8000');
+
+client.on('open', () => {
+	console.log('open');
+	client.send(JSON.stringify({ id: 1 }));
+	// client.send(CANDLE_SUBSCRIPTION);
+});
+client.on('message', data => console.log('message', data));
+// client.on('close', () => console.log('close'));
+client.on('error', err => {
+	console.log('error', err);
+});
+
+// setInterval(getdata, 5000);
 
 const PORT = process.env.PORT || 5000;
+
+https://join.skype.com/TtYOvQ9Khjfj
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
